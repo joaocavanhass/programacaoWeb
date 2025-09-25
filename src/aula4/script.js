0b72a4c058b10bb67fdac2a526ad9415cb20d128
JOAO PEDRO CAVANHAS COLOMBO <joao.pccolombo@senacsp.edu.br>
2025-09-24 21:15:53 -0300

    feat: start js

---
+++ src/aula4/script.js
@@ -0,0 +1,12 @@
+document.getElementById('btnEnviar').innerText = 'Enviar para a nasa';
+
+for (let i = 0; i < 1000000; i++) {
+    
+    console.log(i)
+
+    if (i == 0) {
+        alert('Faça o pix');
+    } else if (i == 1) {
+        alert('Estou te hackeando')
+    } 
+}
